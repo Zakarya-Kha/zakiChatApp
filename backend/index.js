54fb7013dc017e9fc6d7e38fdc17c7c3069a2fdb
Zakarya-Kha <158048203+Zakarya-Kha@users.noreply.github.com>
2024-08-04 11:07:52 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,19 +27,11 @@ app.use(cors(corsOptions));
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/message', messageRoute);
 
-
 // const __dirname = path.resolve();
-
 // app.use(express.static(path.join(__dirname,"../frontend/build")));
-
 // app.get("*", (req,res)=>{
 //      res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
 // })
-
-
-
-
-
 
 server.listen(PORT, () => {
   connectDB();

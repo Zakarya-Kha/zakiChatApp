fffa1ac3fb77c91bcf7d37f4d36b5e19e8a7679c
Zakarya-Kha <158048203+Zakarya-Kha@users.noreply.github.com>
2024-08-04 11:18:56 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import userRoute from './routes/userRoute.js';
 import cookieParser from 'cookie-parser';
 import messageRoute from './routes/messageRoute.js';
 import cors from 'cors';
-import Path from 'path';
+import path from 'path';
 import { app, server } from './socket/socket.js';
 
 dotenv.config();
@@ -27,13 +27,12 @@ app.use(cors(corsOptions));
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/message', messageRoute);
 
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname,"../frontend/build")));
-// app.get("*", (req,res)=>{
-//      res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
-// })
-
-server.listen(PORT, () => {
-  connectDB();
-  console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, async () => {
+  try {
+    await connectDB();
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    process.exit(1);
+  }
 });

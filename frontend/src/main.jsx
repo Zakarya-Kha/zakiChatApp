8702d392cc0a434a09cebd1dded12fc660fa399b
Zakarya-Kha <158048203+Zakarya-Kha@users.noreply.github.com>
2024-08-04 10:48:34 -0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import { persistStore } from "redux-persist";
 
 let persistor = persistStore(store);
 
-export const BASE_URL="http://localhost:8080"
+export const BASE_URL="https://zaki-chat-app.vercel.app"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
